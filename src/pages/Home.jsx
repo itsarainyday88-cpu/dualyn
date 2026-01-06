1a784df187d6cd8f71332ac574d1a7fa53d0ee46
James <lemny.dualyn@gmail.com>
2026-01-06 14:34:36 +0900

    fix main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import Hero from '../components/sections/Hero';
+import Philosophy from '../components/sections/Philosophy';
+import CoreTech from '../components/sections/CoreTech';
+import LNPTech from '../components/sections/LNPTech';
+import DualSystem from '../components/sections/DualSystem';
+import Evidence from '../components/sections/Evidence';
+import Ingredients from '../components/sections/Ingredients';
 
 const Home = ({ lang }) => {
     return (
         <>
             <Hero lang={lang} />
+            <Philosophy lang={lang} />
+            <CoreTech lang={lang} />
+            <LNPTech lang={lang} />
+            <DualSystem lang={lang} />
+            <Evidence lang={lang} />
+            <Ingredients lang={lang} />
         </>
     );
 };
